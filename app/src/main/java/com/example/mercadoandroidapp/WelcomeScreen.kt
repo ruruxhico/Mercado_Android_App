1899f761b11ed6874c21ddfde925ea26c8d1c926
ruruxhico <daniellejanm.15@gmail.com>
2025-06-18 12:51:49 +0800

    added button

--- app/src/main/java/com/example/mercadoandroidapp/WelcomeScreen.kt
+++ app/src/main/java/com/example/mercadoandroidapp/WelcomeScreen.kt
@@ -1,12 +1,16 @@
 package com.example.mercadoandroidapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class WelcomeScreen : AppCompatActivity() {
+    //create layout object variables
+    lateinit var btnProceed: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,19 @@ class WelcomeScreen : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //bind the layout variable to the layout view (control)
+        btnProceed = findViewById<Button>(R.id.btnProceed)
+
+        //add the clock listener to the button
+        btnProceed.setOnClickListener{
+            //nag-dadagdag ng listener ng on click
+            //create an intent variable
+            val intent = Intent(this, Login::class.java)
+
+            //start the activity using the intent
+            startActivity(intent)
+        }
+
     }
 }
