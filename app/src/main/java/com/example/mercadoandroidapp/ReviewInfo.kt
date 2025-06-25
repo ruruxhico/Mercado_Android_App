2550d85b933568dedee82846cd31afa5c80ad48a
ruruxhico <daniellejanm.15@gmail.com>
2025-06-25 12:13:27 +0800

    with review info

--- app/src/main/java/com/example/mercadoandroidapp/ReviewInfo.kt
+++ app/src/main/java/com/example/mercadoandroidapp/ReviewInfo.kt
@@ -8,38 +8,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SignUp : AppCompatActivity() {
-    lateinit var btnGoBack: Button
-    lateinit var btnSignUp: Button
+class ReviewInfo : AppCompatActivity() {
+    lateinit var btnBack: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_review_info)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         //bind the layout variable to the layout view (control)
-        btnGoBack = findViewById<Button>(R.id.btnGoBack)
+        btnBack = findViewById<Button>(R.id.btnBack)
 
         //add the clock listener to the button
-        btnGoBack.setOnClickListener{
+        btnBack.setOnClickListener{
             //nag-dadagdag ng listener ng on click
             //create an intent variable
-            val intent = Intent(this, Login::class.java)
-
-            //start the activity using the intent
-            startActivity(intent)
-        }
-
-        btnSignUp = findViewById<Button>(R.id.btnSignUp)
-
-        btnSignUp.setOnClickListener{
-            //nag-dadagdag ng listener ng on click
-            //create an intent variable
-            val intent = Intent(this, ReviewInfo::class.java)
+            val intent = Intent(this, SignUp::class.java)
 
             //start the activity using the intent
             startActivity(intent)
